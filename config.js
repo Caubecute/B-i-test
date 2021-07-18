6f35b14b253eae5d61aab87c9ed2dfa5e364807c
Caubecute <87606794+Caubecute@users.noreply.github.com>
2021-07-18 20:24:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     introTitle: 'Chị à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    introDesc: `Trái đất vốn lạ thường.
+    Mà sao chị cứ đi nhầm đường.
+    Lạc vào tim em lẻ loi.
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Nhấn vào đây nè chị',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Nhấn vào đây để nhận câu hỏi dễ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích mỗi chị thôi <3',
     btnReply: 'Nhấn vào đây để hoàn thành câu trả lời',
-    reply: 'Yes',
+    reply: 'Tại vì em quá dễ thương',
     mess: 'Em biết mà 🥰. Em thích chị nhiều nhiều 😘😘',
     messLink: 'https://www.facebook.com/le.khoa.3386/'
 }
