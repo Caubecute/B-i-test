40fd15897f8df5621d5d99da432288816fa23bb1
Caubecute <87606794+Caubecute@users.noreply.github.com>
2021-07-19 14:20:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     btnYes: 'Nhấn vào đây để tiếp tục nghe em nói',
     btnNo: 'Nhấn vào đây để thoát',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích mỗi chị thôi <3',
-    btnReply: 'Nhấn vào đây để hoàn thành câu trả lời',
+    btnReply: 'Nhấn vào đây để hoàn thành câu trả lời. Chị điền họ và tên',
     reply: 'Tại vì em quá dễ thương',
     mess: 'Chị phải giữ gìn sức khỏe nhiều hơn nhé. Em ở đây để đợi chị hoàn thành nhiệm vụ rồi về',
     messLink: 'https://www.youtube.com/watch?v=2yDgoBbSXbQ&t=45s'
